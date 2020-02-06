69b7e230c05e541ac965c5e75b587715cab2418e
mphannah <59939101+mphannah@users.noreply.github.com>
2020-02-06 00:47:18 -0500

    Worked on 02-Text Adventure

--- text-adventure/main.py
+++ text-adventure/main.py
@@ -5,6 +5,8 @@ assert sys.version_info >= (3,7), "This script requires at least Python 3.7"
 
 # The game and item description files (in the same folder as this script)
 game_file = 'game.json'
+item_file = 'items.json'
+inventory = []
 
 
 # Load the contents of the files into the game and items dictionaries. You can largely ignore this
@@ -13,27 +15,80 @@ def load_files():
     try:
         __location__ = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))
         with open(os.path.join(__location__, game_file)) as json_file: game = json.load(json_file)
-        return game
+        with open(os.path.join(__location__, item_file)) as json_file: items = json.load(json_file)
+        return (game,items)
     except:
         print("There was a problem reading either the game or item file.")
         os._exit(1)
 
 
-def render(game,current):
+def check_inventory(item):
+    for i in inventory:
+        if i == item:
+            return True
+    return False
+
+def render(game,items,current):
     c = game[current]
-    print("You are at the " + c["name"])
+    print("\nYou are at the " + c["name"])
     print(c["desc"])
+
+    #check for items
+    for item in c["items"]:
+        if not check_inventory(item["item"]):
+            print(item["desc"])
+
+    #display item information
+    for i in inventory:
+        if i in items:
+            if current in items[i]["exits"]:
+                print(items[i]["exits"][current])
+
+    #print available exits
+    print("Available exits:")
+    for e in c["exits"]:
+        print(e["exits"].lower())
 
 def get_input():
     response = input("What do you want to do? ")
     response = response.upper().strip()
     return response
 
-def update(game,current,response):
+def update(game,items,current,response):
+    if response == "INVENTORY":
+        print("You are carrying:")
+        if len(inventory) == 0:
+            print("Nothing")
+            return current
+        else:
+            for i in inventory:
+                print(i.lower())
+        return current
+
     c = game[current]
     for e in c["exits"]:
         if response == e["exit"]:
             return e["target"]
+
+    for item in c["items"]:
+        if response == "GET " + item["item"] and not check_inventory(item["item"]):
+            print(item["take"])
+            inventory.append(item["item"])
+            return current
+
+    for i in inventory:
+        if i in items:
+            for action in items[i]["actions"]:
+                if response == action + " " + i:
+                    print(items[i]["actions"][action])
+
+    if response[0:3] == "GET":
+        print("You can't take that!")
+    elif response in ["NORTH","SOUTH","EAST","WEST","NW","NE","SW","SE","UP","DOWN"]:
+        print("You can't go that way!")
+    else:
+        print("I don't understand what you're trying to do.")
+            
     return current
     
 
@@ -42,10 +97,10 @@ def main():
     current = 'START'  # The starting location
     end_game = ['END']  # Any of the end-game locations
 
-    game = load_files()
+    (game,items) = load_files()
 
     while True:
-        render(game,current)
+        render(game,items,current)
 
         for e in end_game:
             if current == e:
@@ -57,7 +112,7 @@ def main():
         if response == "QUIT" or response == "Q":
             break #break out of the while loop
 
-        current = update(game,current,response)
+        current = update(game,items,current,response)
 
     print("Thanks for playing!")
 
